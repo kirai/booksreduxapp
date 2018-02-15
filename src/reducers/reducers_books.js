d9779dadc0e1e7e3d530048d39b7eece0b8e01fe
kirai <hectorgarcia@gmail.com>
2018-02-15 15:37:58 +0900

    Reducer for the book list

---
+++ src/reducers/reducers_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return[
+        {title: 'Javascript: The Good Parts'},
+        {title: 'Harry poter'},
+        {title: 'The dark tower'},
+        {title: 'Eloquent Ruby'},
+    ];
+}
