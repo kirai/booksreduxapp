01b19a7fda7096d5a2d12f7be4bccf6e83155a9f
kirai <hectorgarcia@gmail.com>
2018-03-09 15:17:21 +0900

    meh

--- src/reducers/reducers_books.js
+++ src/reducers/reducers_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return[
-        {title: 'Javascript: The Good Parts'},
-        {title: 'Harry poter'},
-        {title: 'The dark tower'},
-        {title: 'Eloquent Ruby'}
+        {title: 'Javascript: The Good Parts', pages: 100},
+        {title: 'Harry poter', pages: 120},
+        {title: 'The dark tower', pages: 150},
+        {title: 'Eloquent Ruby', pages: 1}
     ];
 }
